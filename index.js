92fbeda781534cd5a3b4cf013b861d6e7c2dd243
freejacque <freejacque@gmail.com>
2015-10-31 17:31:59 -0400

    completed new index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'node'
+});
+
+connection.query(
+  'SELECT id, content FROM test',
+  function(err, results, fields) {
+    if(err) {
+      console.log('A database error occurred!');
+    } else {
+      console.log(results);
+    }
+    connection.end();
+  }
+);
