367fc2056d0722f948d74a1310476474f6405901
freejacque <freejacque@gmail.com>
2015-10-31 01:14:28 -0400

    created connection and made query

--- index.js
+++ index.js
@@ -2,3 +2,16 @@
 
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root'
+});
+
+connection.query(
+  'SELECT "foo" AS first_field, "bar" AS second_field',
+  function (err, results, fields) {
+    console.log(results);
+    connection.end();
+  }
+);
