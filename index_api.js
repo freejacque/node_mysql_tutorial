f7cdfc4f0717b4038cf44243ff67e70e61380049
freejacque <freejacque@gmail.com>
2015-11-02 16:51:04 -0500

    created vars query, connection, and mysql

--- index_api.js
+++ index_api.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'node'
+});
+
+var query = connection.query('SELECT id, content FROM test');
+
